358d07a46ed3e5ec5b6dd9e623cab5d07fb98dcd
Kio-reki <ikrayz1ei@hotmail.com>
2019-05-26 16:17:48 -0700

    Added dashing feature

--- Assets/_Scripts/PlayerControl.cs
+++ Assets/_Scripts/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerControl : MonoBehaviour
 {
 
     [Header("Movement")]
+    public bool facingRight = true;
     public float maxSpeed = 8f;
     public float currentSpeed;
     public float airSpeed;
@@ -18,6 +19,19 @@ public class PlayerControl : MonoBehaviour
     bool isGrappling = false;
     public int jumpCount = 0;
     //public float sprintSpeed = 50f;
+
+    [Space]
+
+    public bool canDash = true;
+    public float dashDistance = 5;
+    public float superDashDistance = 10;
+    //public float dashTime;
+    //public float startDashTime;
+    //public float rightDashSpeed;
+    //public float leftDashSpeed;
+
+    //public int direction;
+
 
     [Space]
 
@@ -34,37 +48,68 @@ public class PlayerControl : MonoBehaviour
     public Vector2 savedVelocity;
 
     private Rigidbody2D rb;
-    //Animator anim;
+    Animator anim;
 
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // anim = GetComponent<Animator>();
-        PlayerPosition = transform;
+        anim = GetComponent<Animator>();
+        //dashTime = startDashTime;
     }
-
-    private void Update()
-    {
-        if (grounded && jumpCount > 0)
-        {
-            jumpCount = 0;
-        }
-    }
-
 
     void FixedUpdate()
     {
+
+        PlayerPosition = transform;
+
+        //GROUND CHECK
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
         //anim.SetBool("grounded", grounded);
 
-        //anim.SetFloat("vSpeed", rb.velocity.y);
+        if (grounded)
+        {
+            canDash = true;
+        }
 
-
+        //MOVEMENT
         float move = Input.GetAxis("Horizontal");
-        //anim.SetFloat("Speed", Mathf.Abs(move));
+        anim.SetFloat("Speed", Mathf.Abs(move));
         rb.velocity = new Vector2((move * maxSpeed), rb.velocity.y);
+
+        //FACING DIRECTION CHECK
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+        //FUNCTIONS
+        PlayerJump();
+        PlayerDash();
+        //SuperDash();
+
+
+
+
+
+
+       
+       
+
+    }
+    //VOID FUNCTIIONS
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void PlayerJump()
+    {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -83,26 +128,39 @@ public class PlayerControl : MonoBehaviour
                     rb.AddForce(new Vector2(0, jumpForce));
                 }
             }
-
-
-
-            /* if (grounded && Input.GetKeyDown(KeyCode.Space))
-             {
-                 rb.AddForce(new Vector2(0, jumpForce));
-                 jumpCount += 1;
-             }
-
-             if (!grounded && Input.GetKeyDown(KeyCode.Space))
-             {
-                 rb.AddForce(new Vector2(0, dblJumpForce));
-             }*/
-
-
-
-
-
-
-
         }
     }
+
+    void PlayerDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            if (facingRight)
+            {
+                PlayerPosition.position = transform.TransformPoint(dashDistance, 0, 0);
+                canDash = false;
+            }
+            else if (!facingRight)
+            {
+                PlayerPosition.position = transform.TransformPoint(dashDistance, 0, 0);
+                canDash = false;
+            }
+        }
+
+    }
+
+    /*void SuperDashInit()
+    {
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                rb.velocity = new Vector2(0, 0);
+                Invoke("SuperDash", 0.5f);
+            }
+        }
+    }
+    void SuperDash()
+    {
+        rb.velocity = new Vector2(superDashDistance, 0);
+    }*/
 }
